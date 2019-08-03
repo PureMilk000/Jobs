9fe9274cf700b5f8ab19dbe1caa0a773b88ef032
Mark <1817956299@qq.com>
2019-08-03 14:25:19 +0800

    01

---
+++ exam/exam/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  exam
+//
+//  Created by mark on 2019/8/3.
+//  Copyright © 2019年 mark. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
